3f1f5c9fccbda40d7b98eb8ebc3811fb677a54c2
Arni Magnusson <thisisarni@gmail.com>
2023-03-22 11:58:55 +1100

    Merge frq and fdesc

--- fisheries/model.R
+++ fisheries/model.R
@@ -1,9 +1,19 @@
 ## Run analysis, write model results
 
-## Before:
-## After:
+## Before: fdesc.csv, frq_yft.csv (data)
+## After:  frq_yft.csv (model)
 
 library(TAF)
 
 mkdir("model")
 
+# Read files
+fdesc <- read.taf("data/fdesc.csv")
+frq.yft <- read.taf("data/frq_yft.csv")
+
+# Merge length frequencies and fishery descriptions
+frq.yft <- merge(frq.yft, fdesc)
+frq.yft <- frq.yft[c("fishery.id", "fishery", "region", "length", "freq")]
+
+# Write table
+write.taf(frq.yft, dir="model")
