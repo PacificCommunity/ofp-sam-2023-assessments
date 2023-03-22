f506743b6c309f8e20956afd9b7b84701c0f2ddb
Arni Magnusson <thisisarni@gmail.com>
2023-03-22 11:31:43 +1100

    Extract MFCL data and write to CSV files

--- fisheries/data.R
+++ fisheries/data.R
@@ -1,9 +1,26 @@
 ## Preprocess data, write TAF data tables
 
-## Before:
-## After:
+## Before: fdesc.txt, yft.frq (boot/data)
+## After:  fdesc.csv, yft_frq.csv (data)
 
 library(TAF)
+library(FLR4MFCL)
 
 mkdir("data")
 
+# Read files
+yft.frq <- read.MFCLFrq("boot/data/yft.frq")
+fdesc <- read.table("boot/data/fdesc.txt", header=TRUE)
+
+# Extract length frequencies
+yft.frq <- freq(yft.frq)
+yft.frq <- aggregate(freq~length+fishery, yft.frq, sum)
+names(yft.frq)[names(yft.frq)=="fishery"] <- "fishery.id"
+
+# Rename fishery columns
+names(fdesc)[names(fdesc)=="num"] <- "fishery.id"
+names(fdesc)[names(fdesc)=="code"] <- "fishery"
+
+# Write TAF tables
+write.taf(yft.frq, dir="data")
+write.taf(fdesc, dir="data")
