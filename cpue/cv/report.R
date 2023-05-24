f2afb007282ac840d8d285ee439f3b40f41c3bd0
Arni Magnusson <thisisarni@gmail.com>
2023-05-24 14:45:12 +1100

    Plot CPUE, sigma and format table

--- cpue/cv/report.R
+++ cpue/cv/report.R
@@ -1,9 +1,35 @@
 ## Prepare plots and tables for report
 
-## Before:
-## After:
+## Before: cpue.csv (data), sigma.csv (output)
+## After:  cpue.png, sigma.csv, sigma.png (report)
 
 library(TAF)
+source("utilities.R")
 
 mkdir("report")
 
+# Read CPUE data and sigma
+cpue <- read.taf("data/cpue.csv")
+sigma <- read.taf("output/sigma.csv")
+
+# Split by region
+splits <- split(cpue, cpue$area)
+
+# Plot obs and fit
+taf.png("cpue")
+par(mfrow=c(3,3), plt=c(0.20,0.95,0.15,0.90))
+invisible(sapply(splits, plot.cpue))
+dev.off()
+
+# Plot sigmas
+taf.png("sigma")
+par(mfrow=c(1,1))
+barplot(sigma~area, sigma)
+abline(h=median(sigma$sigma), lty=2, lwd=2, col=2)
+dev.off()
+
+# Round sigmas
+sigma <- round(sigma, 3)
+
+# Write TAF table
+write.taf(sigma, dir="report")
--- cpue/cv/utilities.R
+++ cpue/cv/utilities.R
@@ -1,3 +1,14 @@
+plot.cpue <- function(x)
+{
+  plot(obs~time, data=x, ylim=lim(c(x$obs,x$fit)), ann=FALSE, col="gray")
+  lines(fit~time, data=x)
+  par(new=TRUE)
+  frame()
+  sigma <- formatC(sigma.cpue(x), format="f", digits=3)
+  text(0.95, 0.95, x$area[1], adj=1, font=2)
+  text(0.95, 0.85, paste("sigma =", sigma), adj=1)
+}
+
 sigma.cpue <- function(x)
 {
   obs <- x$obs[!is.na(x$obs)]
