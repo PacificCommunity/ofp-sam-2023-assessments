8694919790c543424a5260763e7c652a5547e9ce
Arni Magnusson <thisisarni@gmail.com>
2023-05-24 14:24:25 +1100

    Copy results to output

--- cpue/cv/output.R
+++ cpue/cv/output.R
@@ -1,9 +1,10 @@
 ## Extract results of interest, write TAF output tables
 
-## Before:
-## After:
+## Before: sigma.csv (model)
+## After:  sigma.csv (output)
 
 library(TAF)
 
 mkdir("output")
 
+cp("model/sigma.csv", "output")
