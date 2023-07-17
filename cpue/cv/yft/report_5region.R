fa4a31c22ee9317b9ef1ee111c8dfe289fb338d8
Arni Magnusson <thisisarni@gmail.com>
2023-07-17 14:01:01 +1100

    Calculate sigma and MFCL flags for 5 regions

--- cpue/cv/yft/report_5region.R
+++ cpue/cv/yft/report_5region.R
@@ -1,14 +1,15 @@
 ## Calculate 5-region sigmas and flag values
 
-## Before: mfcl_model.csv, sigma_model.csv (output)
+## Before: sigma_model.csv (output)
 ## After:  mfcl_five.csv, sigma_five.csv (report)
 
 library(TAF)
 source("utilities.R")  # convert.cv
 
+mkdir("report")
+
 # Read sigma and MFCL flag values
 sigma <- read.taf("output/sigma_model.csv")
-mfcl <- read.taf("report/mfcl_model.csv")
 
 # Map old regions to new
 map <- read.table(text="
@@ -31,12 +32,6 @@ names(sigma.five) <- c("region", "sigma")
 
 # Convert to MFCL flag values
 mfcl.five <- data.frame(region=1:5, value=convert.cv(sigma.five$sigma))
-
-# Alternatively, we could average the 9-region flag values
-alt.five <- data.frame(map, value=unlist(mfcl))
-alt.five <- aggregate(value~new, alt.five, function(x) round(mean(x)))
-names(alt.five) <- c("region", "value")
-# but we won't use that
 
 # Write TAF tables
 write.taf(sigma.five, dir="report")
